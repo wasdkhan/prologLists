449e00faa1ba6c93a85c7e60282d4810e4b3d0d7
Waseem Khan <waseemkhan96@outlook.com>
2016-01-29 21:00:16 -0500

    added problems 4 and 5

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import re
 problemNumber = int(raw_input('Enter Problem Number: '))
-if problemNumber == 1:
+def listinput():
   input = raw_input('Enter your list: ')
   elements = re.findall(r'[\w]+',input)
   list = []
   for elem in elements: list.append(elem)
+  return list
+list = listinput()
+if problemNumber == 1:
   print list[len(list)-1]
+if problemNumber == 2:
+  print list[len(list)-2]
+if problemNumber == 3:
+  index = int(raw_input('Please Enter List Element Number: '))
+  print list[index-1]
+if problemNumber == 4:
+  print len(list)
+if problemNumber == 5:
+  list.reverse()
+  print list
