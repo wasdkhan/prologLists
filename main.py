e1987069be4e1f0b10aa17aa41e66a8d0aebdac1
Waseem Khan <waseemkhan96@outlook.com>
2016-01-29 21:09:26 -0500

    added def list and shorteneed 3 prompt

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import re
 problemNumber = int(raw_input('Enter Problem Number: '))
 def listinput():
   input = raw_input('Enter your list: ')
-  elements = re.findall(r'[\w]+',input)
   list = []
-  for elem in elements: list.append(elem)
-  return list
+  if input == 'def': 
+    list = ['a','b','c','d','e']
+    print list
+    return list
+  else:
+    elements = re.findall(r'[\w]+',input)
+    for elem in elements: list.append(elem)
+    return list
 list = listinput()
 if problemNumber == 1:
   print list[len(list)-1]
 if problemNumber == 2:
   print list[len(list)-2]
 if problemNumber == 3:
-  index = int(raw_input('Please Enter List Element Number: '))
+  index = int(raw_input('Enter list element position: '))
   print list[index-1]
 if problemNumber == 4:
   print len(list)
